12b9a17924f566eed57ffed111c7a315fc76b081
sudeep <sudeep.connect19@gmail.com>
2025-03-08 22:37:16 +0530

    added few more programmes

---
+++ src/main/java/org/sudeep/arrays/FindFirstAndLastIndex.java
@@ -0,0 +1,65 @@
+package org.sudeep.arrays;
+
+public class FindFirstAndLastIndex {
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 6;
+
+        int firstIndex = findFirstIndex(nums, target);
+        int lastIndex = findLastIndex(nums, target);
+
+        int[] ans = new int[2];
+        ans[0] = firstIndex;
+        ans[1] = lastIndex;
+
+        for(int num:ans){
+            System.out.println(num+" ");
+        }
+    }
+
+
+    private static int findFirstIndex(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        int ans = -1;
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (target == nums[mid]) {
+                ans = mid;
+                high = mid - 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+
+
+    private static int findLastIndex(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (target == nums[mid]) {
+                ans = mid;
+                low = mid + 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
